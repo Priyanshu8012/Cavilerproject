e849ba3b612d2a4d3ae0361ce130d793ddde9453
Priyanshu8012 <141941989+Priyanshu8012@users.noreply.github.com>
2026-02-09 23:42:23 +0530

    Fix: Explicit .env path

--- cavilerbackend/server.js
+++ cavilerbackend/server.js
@@ -1,5 +1,6 @@
 // server.js
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
